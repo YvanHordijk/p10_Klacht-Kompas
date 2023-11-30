232b6c5fe5541cb242e94828f49555eea1264ca7
YvanHordijk <91611502+YvanHordijk@users.noreply.github.com>
2023-11-30 08:47:07 +0100

    homepage and header

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="header">
+<ul>
+  <li><a href='index.php'>Home</a></li>
+  <li><a>complaint</a></li>
+  <li><a>Contact</a></li>
+  <li><a>inloggen</a></li>
+</ul>
+</div>
+</body>
+</html>
