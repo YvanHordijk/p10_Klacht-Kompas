e2c29566a2c9342bb46130560deaf8e607d60e45
YvanHordijk <91611502+YvanHordijk@users.noreply.github.com>
2023-12-13 12:27:58 +0100

    added comments and made conn.php

--- Leaflet/Map.php
+++ Leaflet/Map.php
@@ -1,7 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
 <head>
+//code to load map.js into this file
 <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin=""/>
 <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
 <style>#map { height: 600px; }</style>
+
 </head>
 <body>
     <div id="map"></div>
